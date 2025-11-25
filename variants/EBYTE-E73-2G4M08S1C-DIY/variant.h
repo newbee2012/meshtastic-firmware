96ffd229076550fa6de5d193e95dab2640c9e2ec
newbee2012 <newbee2012@126.com>
2025-11-25 22:31:28 +0800

    delete temp code

--- variants/EBYTE-E73-2G4M08S1C-DIY/variant.h
+++ variants/EBYTE-E73-2G4M08S1C-DIY/variant.h
@@ -1,20 +1,3 @@
-/*
- Copyright (c) 2014-2015 Arduino LLC.  All right reserved.
- Copyright (c) 2016 Sandeep Mistry All right reserved.
- Copyright (c) 2018, Adafruit Industries (adafruit.com)
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- See the GNU Lesser General Public License for more details.
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 
 #ifndef _VARIANT_EBYTE_E73_2G4M08S1C_DIY
 #define _VARIANT_EBYTE_E73_2G4M08S1C_DIY
@@ -168,6 +151,9 @@ static const uint8_t A4 = PIN_A4;
 
 //Buzzer
 #define PIN_BUZZER (0 + 2)
+
+// 中文字体支持
+#define OLED_MESSAGE_ZH 1
 
 #ifdef __cplusplus
 }
